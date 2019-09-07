8f953065de4b3b62b78289a0e5ebe307443c4e1a
zhayh <zhayh.nj@gmail.com>
2019-09-07 12:12:47 +0800

    完成主页面的底部导航

--- app/src/main/java/edu/niit/android/course/MainActivity.java
+++ app/src/main/java/edu/niit/android/course/MainActivity.java
@@ -1,14 +1,48 @@
 package edu.niit.android.course;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.title_toolbar);
+        toolbar.setTitle("课程");
+        setSupportActionBar(toolbar);
+
+        // 设置回退按钮，及点击事件
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true); // 设置返回键
+            actionBar.setHomeButtonEnabled(true);    // 设置是否是首页
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
