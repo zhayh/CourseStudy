332c3515846ee64b1f700188fa825d973f019392
zhayh <zhayh.nj@gmail.com>
2019-10-14 11:07:48 +0800

    完成用户信息的文件内部和外部存储功能

--- app/src/main/java/edu/niit/android/course/activity/UserInfoActivity.java
+++ app/src/main/java/edu/niit/android/course/activity/UserInfoActivity.java
@@ -1,13 +1,7 @@
 package edu.niit.android.course.activity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,6 +10,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,7 +18,24 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -43,8 +55,6 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
     private static final int MODIFY_SIGNATURE = 2;
     private static final int PICK_IMAGE = 3;
 
-    private static final int REQUEST_PERMISSION_READ = 11;
-
     // 1. 定义界面上的控件对象
     private TextView tvNickname, tvSignature, tvUsername, tvSex;
     private RelativeLayout nicknameLayout, signatureLayout, sexLayout, headLayout;
@@ -54,6 +64,12 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
     private String spUsername;
     private UserInfo userInfo;
     private UserInfoService service;
+
+    // 3. 文件存储相关的变量
+
+
+
+    private static final int REQUEST_READ_PHOTO = 13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,20 +84,28 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
         // 5. 获取所有控件对象，设置监听器（必须）
         initView();
     }
+
     private void initData() {
         spUsername = SharedUtils.readValue(this, "loginUser");
 
         service = new UserInfoServiceImpl(this);
-        userInfo = service.get(spUsername);
-        if(userInfo == null) {
+        userInfo = service.get(spUsername);     // 从数据库读取
+        userInfo = readFromInternal();          // 从内部存储读取
+        userInfo = readPrivateExStorage();      // 从外部的私有存储读取
+        userInfo = readPublicExternalStorage(); // 从外部的公有存储读取
+        if (userInfo == null) {
             userInfo = new UserInfo();
             userInfo.setUsername(spUsername);
             userInfo.setNickname("课程助手");
             userInfo.setSignature("课程助手");
             userInfo.setSex("男");
             service.save(userInfo);
+            saveToInternal(userInfo);
+            savePrivateExStorage(userInfo);
+            savePublicExternalStorage(userInfo);
         }
     }
+
     private void initView() {
         // 1. 获取控件对象
         tvUsername = findViewById(R.id.tv_username);
@@ -126,38 +150,10 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.rl_head:
                 modifyHeadIcon();
+                break;
         }
     }
 
-    private void modifyHeadIcon() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if(ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        REQUEST_PERMISSION_READ);
-            } else {
-                choosePhoto();
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_PERMISSION_READ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            choosePhoto();
-        } else {
-            Toast.makeText(this, "申请读取SD卡的权限未允许", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    void choosePhoto(){
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");//相片类型
-        startActivityForResult(intent, PICK_IMAGE);
-    }
-    
     private void modifyNickname() {
         // 1. 获取已有的内容
         String nickname = tvNickname.getText().toString();
@@ -195,75 +191,269 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
                 .setTitle("性别")
                 .setSingleChoiceItems(datas, selected,
                         new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String sex = datas[which];
-                        tvSex.setText(sex);
-                        userInfo.setSex(sex);
-                        service.modify(userInfo);
-                        dialog.dismiss();
-                    }
-                }).show();
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String sex = datas[which];
+                                tvSex.setText(sex);
+                                userInfo.setSex(sex);
+                                service.modify(userInfo);
+                                saveToInternal(userInfo);
+                                dialog.dismiss();
+                            }
+                        }).show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // 1. 对空数据、返回异常做判断
-        if(data == null || resultCode != RESULT_OK) {
+        if (data == null || resultCode != RESULT_OK) {
             Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
             return;
         }
         // 2. 根据requestCode进行对应的保存
         // 2.1 获取data数据
-        if(requestCode == 1) {
-            // 2.2 设置userInfo对应的属性值，更新界面对应的控件内容
-            String value = data.getStringExtra("nickname");
-            tvNickname.setText(value);
-            userInfo.setNickname(value);
-        } else if(requestCode == 2) {
-            String value = data.getStringExtra("signature");
-            tvSignature.setText(value);
-            userInfo.setSignature(value);
+        switch (requestCode) {
+            case 1:
+                // 2.2 设置userInfo对应的属性值，更新界面对应的控件内容
+                String newNick = data.getStringExtra("nickname");
+                if (!TextUtils.isEmpty(newNick)) {
+                    tvNickname.setText(newNick);
+
+                    // 2.3 保存到数据库
+                    userInfo.setNickname(newNick);
+                    service.modify(userInfo);
+
+                    // 2.3 保存到内部存储
+                    saveToInternal(userInfo);
+                }
+                break;
+            case 2:
+                String newSignature = data.getStringExtra("signature");
+                if (!TextUtils.isEmpty(newSignature)) {
+                    tvSignature.setText(newSignature);
+                    userInfo.setSignature(newSignature);
+                    service.modify(userInfo);
+                    saveToInternal(userInfo);
+                }
+                break;
+            case PICK_IMAGE:
+                try {
+                    Uri uri = data.getData();
+                    Bitmap header = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                    //
+                    ivHead.setImageBitmap(header);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
         }
-        // 2.3 保存到数据库
-        service.modify(userInfo);
     }
 
-    //    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if(data == null || resultCode != RESULT_OK) {
-//            Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//        switch (requestCode) {
-//            case 1:
-//                String newNick = data.getStringExtra("nickname");
-//                if(!TextUtils.isEmpty(newNick)) {
-//                    tvNickname.setText(newNick);
-//                    userInfo.setNickname(newNick);
-//                    service.modify(userInfo);
-//                }
-//                break;
-//            case 2:
-//                String newSignature = data.getStringExtra("signature");
-//                if(!TextUtils.isEmpty(newSignature)) {
-//                    tvSignature.setText(newSignature);
-//                    userInfo.setSignature(newSignature);
-//                    service.modify(userInfo);
-//                }
-//                break;
-//            case PICK_IMAGE:
-//                try {
-//                    Uri uri = data.getData();
-//                    Bitmap header = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
-//                    ivHead.setImageBitmap(header);
-//                } catch (FileNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                break;
-//        }
-//    }
+    /*
+        文件读写的流程
+        1. 打开文件输入或输出流
+        2. 创建BufferedReader或BufferedWriter对象读写文本文件
+        3. 读写数据
+        4. 关闭输入输出流
+     */
+    private static final String FILE_NAME = "userinfo.txt";
+    // 2. 内部存储
+    // 保存
+    private void saveToInternal(UserInfo userInfo) {
+        // 内部存储目录：data/data/包名/files/
+        try {
+            // 1. 打开文件输出流
+            FileOutputStream out = this.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            // 2. 创建BufferedWriter对象
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+            // 3. 写入数据
+            writer.write(JSON.toJSONString(userInfo));
+            // 4. 关闭输出流s
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 读取
+    private UserInfo readFromInternal() {
+        UserInfo userInfo = null;
+        try {
+            FileInputStream in = this.openFileInput(FILE_NAME);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String data = reader.readLine();
+            userInfo = JSON.parseObject(data, UserInfo.class);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userInfo;
+    }
+
+    // 3. 外部私有存储
+    // 保存
+    private void savePrivateExStorage(UserInfo userInfo) {
+        // 外部私有存储目录：/storage/emulated/0/Android/data/包名/files/
+        try {
+            // 1. 打开文件输出流
+            File file = new File(getExternalFilesDir(""), FILE_NAME);
+            FileOutputStream out = new FileOutputStream(file);
+            // 2. 创建BufferedWriter对象
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+            // 3. 写入数据
+            writer.write(JSON.toJSONString(userInfo));
+            writer.flush();
+            // 4. 关闭输出流
+            writer.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 读取
+    private UserInfo readPrivateExStorage() {
+        // 外部私有存储目录：/storage/emulated/0/Android/data/包名/files/
+        UserInfo userInfo = null;
+        try {
+            File file = new File(getExternalFilesDir(""), FILE_NAME);
+            if(!file.exists()) {
+                return null;
+            }
+            FileInputStream in = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String data = reader.readLine();
+            userInfo = JSON.parseObject(data, UserInfo.class);
+            reader.close();
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userInfo;
+    }
+    // 4. 外部公有存储
+    // Android 6.0的权限申请回调标识
+    private static final int REQUEST_READ_USERINFO = 101;
+    private static final int REQUEST_WRITE_USERINFO = 102;
+    private void savePublicExternalStorage(UserInfo userInfo) {
+        // 外部私有存储目录：/storage/emulated/0/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_WRITE_USERINFO);
+                return;
+            }
+        }
+        saveUserInfo(userInfo);
+    }
+    // 读取
+    private UserInfo readPublicExternalStorage() {
+        // 外部私有存储目录：/storage/emulated/0/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_READ_USERINFO);
+                return userInfo;
+            }
+        }
+        return readUserInfo();
+    }
+
+    // 读写SD卡权限的请求回调
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // 1. 判断申请的结果
+        if (grantResults.length == 0 ||
+                grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "申请权限被拒绝，无法执行操作",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 2. 如果被允许，根据requestCode进行相应的处理
+        if (requestCode == REQUEST_READ_USERINFO) {
+            userInfo = readUserInfo();
+        } else if (requestCode == REQUEST_WRITE_USERINFO) {
+            saveUserInfo(userInfo);
+        } else if (requestCode == REQUEST_READ_PHOTO) {
+            choosePhoto();
+        }
+    }
+
+    private void saveUserInfo(UserInfo userInfo) {
+        // 检查SD卡的挂载情况
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state)) {
+            Toast.makeText(this, "无法读取SD卡", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            // 1. 打开文件输出流
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), FILE_NAME);
+            FileOutputStream out = new FileOutputStream(file);
+            // 2. 创建BufferedWriter对象
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+            // 3. 写入数据
+            writer.write(JSON.toJSONString(userInfo));
+            writer.flush();
+            // 4. 关闭输出流
+            writer.close();
+            out.close();
+            Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "保存失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private UserInfo readUserInfo() {
+        UserInfo userInfo = null;
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), FILE_NAME);
+        try {
+            FileInputStream in = new FileInputStream(file);
+            int length = in.available();
+            byte[] data = new byte[length];
+            int len = in.read(data);
+            userInfo = JSON.parseObject(data, UserInfo.class);
+            Toast.makeText(this, "读取成功", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "读取失败", Toast.LENGTH_SHORT).show();
+        }
+        return userInfo;
+    }
+
+    private void modifyHeadIcon() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_READ_PHOTO);
+                return;
+            }
+        }
+        choosePhoto();
+    }
+
+    void choosePhoto() {
+        // Intent.ACTION_PICK：隐性的Intent
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");//相片类型
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+
 }
